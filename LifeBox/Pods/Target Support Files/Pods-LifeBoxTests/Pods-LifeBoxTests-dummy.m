483a4e3cfd4e56959f67a1e59b1e6db66c9e296a
UbunGit <296019487@qq.com>
2019-08-11 23:07:06 +0800

    新建项目

---
+++ LifeBox/Pods/Target Support Files/Pods-LifeBoxTests/Pods-LifeBoxTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_LifeBoxTests : NSObject
+@end
+@implementation PodsDummy_Pods_LifeBoxTests
+@end
---
+++ LifeBox/Pods/Target Support Files/Pods-LifeBoxUITests/Pods-LifeBoxUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_LifeBoxUITests : NSObject
+@end
+@implementation PodsDummy_Pods_LifeBoxUITests
+@end
