c6a270875ae03434cf1501d03993147bdc134d60
admin <admin@admin.local>
2020-03-16 18:37:14 +0800

    添加后台app

---
+++ BusinseeLifeBox/BusinseeLifeBox/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  BusinseeLifeBox
+//
+//  Created by admin on 2020/3/16.
+//  Copyright © 2020 admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ BusinseeLifeBox/BusinseeLifeBox/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BusinseeLifeBox
+//
+//  Created by admin on 2020/3/16.
+//  Copyright © 2020 admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
